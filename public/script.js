46130a2be8ac464eb1c1973e77a04b64d7290bbf
agentLakh <lesgenies62@gmail.com>
2025-03-01 02:24:04 +0000

    Prototype todo list

--- public/script.js
+++ public/script.js
@@ -1,3 +1,53 @@
+const newTask = document.getElementById("input");
+const addTask = document.getElementById("add-btn");
+const taskList = document.getElementById("task-list");
+
+addTask.addEventListener("click", () => {
+    addToList();
+});
+
+
+newTask.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        addToList();
+    }
+});
+
+function addToList() {
+    let text = newTask.value;
+    if (text) {
+    
+        const listItem = document.createElement("li");
+        listItem.className = "task-item";
+        const checkbox = document.createElement("button");
+        checkbox.className = "btn-check";
+        checkbox.innerHTML = `<i class="fa-solid fa-check"><i/>`;
+        const span = document.createElement("span");
+        span.className = "text";
+        span.textContent = text;
+        const closeButton = document.createElement("button");
+        closeButton.className = "btn-close";
+        closeButton.innerHTML = `<i class="fa-solid fa-xmark"><i/>`;
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(span);
+        listItem.appendChild(closeButton);
+
+        taskList.appendChild(listItem);
+
+        newTask.value = "";
+
+
+        closeButton.addEventListener("click", () => {
+            taskList.removeChild(listItem);
+        });
+
+        checkbox.addEventListener("click", () => {
+            listItem.classList.toggle("completed");
+        });
+    }
+}
+
 const button = document.getElementById("changeMode")
 const icon = document.querySelector(".fa-sun")
 button.addEventListener("click", () => {
