f96a5f8eb73dfd7739f61d18dee671a636ffc738
binetougaye <fabigaye3@gmail.com>
2025-03-02 12:57:27 +0000

    wip: mood du jour

--- public/script.js
+++ public/script.js
@@ -39,9 +39,65 @@ async function getHijriDate() {
         const weekday = data?.data.hijri.weekday.en
         hijriDate.innerText = weekday + " " + day + " " + month + " " + year
         dateDuJour.innerText = day
+        console.log(data.data);
+
 
     } catch (error) {
         console.log(error);
     }
 }
 getHijriDate()
+// Obtenir les Heures de prières
+const api = "https://api.aladhan.com/v1/hijriCalendarByAddress/1446/9?address=Dakar,Senegal"
+const prayerTimes = document.getElementById("prayer_times")
+async function getPrayerTimes() {
+    try {
+        const response = await fetch(api)
+        const data = await response.json()
+        console.log(data.data.map((item) => item.timings));
+        const test = data.data.map((item) => item.timings)
+        test.filter((item, index) => item.length === 7)
+
+        // console.log(data.data.filter((item, index) => item.length <= 7));
+        // const teste = test.map((item, index) => item);
+        // console.log(teste);
+        // console.log(teste.filter((item, index) => item.length <= 7));
+
+
+
+
+
+
+    } catch (error) {
+        console.log(error);
+
+
+    }
+}
+getPrayerTimes()
+// Recommandation en fonction de l'humeur
+const sadness = document.getElementById("sad")
+const texte = document.getElementById("text")
+const audio = document.getElementById("audio")
+// mettre un numéro qui varie en fonction de la sourate de 1 à 114
+const num = 93
+const apiSad = `https://api.alquran.cloud/v1/surah/${num}/en.asad`
+async function Test() {
+    try {
+        const response = await fetch(apiSad);
+        const data = await response.json();
+        return data.data.ayahs[3].text;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+sadness.addEventListener("click", async () => {
+    const text = await Test();
+    if (text) {
+        texte.innerText = text
+    } else {
+        texte.innerText = ""
+    }
+});
